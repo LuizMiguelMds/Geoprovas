282bf8c09da0882a60e4e4539e1e7d7e3b043ffd
Luiz Miguel <luizmiguelmds2004@gmail.com>
2025-04-10 17:14:14 -0300

    função classificador

--- backend/classificador_ia.py
+++ backend/classificador_ia.py
@@ -1,3 +1,8 @@
 # Metodo para classificação
 from transformes import pipeline
 
+classificador = pipeline(
+    "text-classification",
+    model = "neuralmind/bert-base-portuguese-cased",
+    function_to_apply="none"
+)
